a1b782b1895672d4c5e1ce69eb98fa00e60b95ae
Benjamin Van Ryseghem <benvr@wanadoo.fr>
2013-01-31 21:30:07 +0100

    Pharo ver 1.5 = add version check to prevent from downloading a new image each time

--- pharo/newImage.rb
+++ pharo/newImage.rb
@@ -2,14 +2,75 @@
 require 'fileutils'
 require 'find'
 
-new_image = false
-args = []
+IO.popen('pbcopy', 'w').puts $*
 
+def openImage(string, full_path)
+    puts string
+    `open "#{full_path}"`
+    exit(0)
+end
+
+def openExistingImage(full_path, new_image)
+    if !new_image
+        openImage("Already existing Pharo image\n#{full_path}", full_path)
+    end
+end
+
+def splitArguments(s)
+    args = []
+    s.scan(/\"([^\"]+)\"/){|i| args << i[0]}
+    return args
+end
+
+def retieveCurrentVersion()
+    code = IO.popen("curl https://ci.inria.fr/pharo/job/Pharo-2.0/lastSuccessfulBuild/")
+    code = code.readlines.join[0..-2]
+    prefix = '      </h1><div><div id="description"><div>2.0 #'
+    line = ''
+    code.each_line { | l |
+        if l.chomp.start_with?(prefix)
+            line = l
+            break
+        end
+    }
+    size = prefix.size
+    return line[size, 5]
+end
+
+def downloadNewVersion(path, destination)
+    imageUrl = "https://ci.inria.fr/pharo/view/Pharo-2.0/job/Pharo-2.0/lastSuccessfulBuild/artifact/Pharo.zip"
+
+    backup = ''
+    Dir.glob(File.join(path, '*.zip')).each do |file|
+        backup = file
+    end
+    backup = File.basename(backup,File.extname(backup))
+
+    current_version = retieveCurrentVersion()
+    if backup.eql?(current_version)        
+        `cp "#{path}/#{backup}.zip" "#{path}/artifact.zip"`
+    else
+        `curl --retry 2 --connect-timeout 3 --anyauth "#{imageUrl}" --output "#{path}/artifact.zip" &&  cp -f "#{path}/artifact.zip" "#{path}/#{current_version}.zip" && rm "#{path}/#{backup}.zip" || cp "#{path}/#{backup}.zip" "#{path}/artifact.zip"`
+    end
+
+    `unzip -xo "#{path}"/artifact.zip -d "#{destination}"`
+    `rm "#{path}"/artifact.zip`
+end
+
+def moveFiles(destination, name)
+    Dir.glob(File.join(destination, '*')).each do |file|
+        if File.basename(file,File.extname(file)) != name
+            FileUtils.mv file, File.join(destination, name + File.extname(file)), :force=>true
+        end
+    end
+end
+
+new_image = false
 s = $*[0]
-s.scan(/\"([^\"]+)\"/){|i| args << i[0]}
+args = splitArguments(s)
+
 
 name_index = args.size-3
-
 if args.size == 4 && args[0].eql?('-f' )
    new_image = true
 end
@@ -20,31 +81,8 @@ dir = args[name_index+2]
 destination = path +"/"+dir
 full_path = destination+"/"+name+".image"
 
-if !new_image
-    puts "Already existing Pharo image\n#{full_path}"
-    `open "#{full_path}"`
-    exit(0)
-end
+openExistingImage(full_path, new_image)
+downloadNewVersion(path, destination)
+moveFiles(destination, name)
 
-# ===========================================================================
-
-version  = '2.0'
-tmp      = `mktemp -d -t pharo`.chomp
-imageUrl = "https://ci.inria.fr/pharo/view/Pharo-#{version}/job/Pharo-#{version}/lastSuccessfulBuild/artifact/Pharo.zip"
-
-# ===========================================================================
-`cd "#{path}" && curl --retry 2 --connect-timeout 3 --anyauth "#{imageUrl}" --output "artifact.zip" &&  cp -f "#{path}/artifact.zip" "#{path}/backup.zip"  || cp "#{path}/backup.zip" "#{path}/artifact.zip"`
-`unzip -xo "#{path}"/artifact.zip -d "#{destination}"`
-
-Dir.glob(File.join(destination, '*')).each do |file|
-    if File.basename(file,File.extname(file)) != name
-        FileUtils.mv file, File.join(destination, name + File.extname(file)), :force=>true
-    end
-end
-
-`rm "#{path}"/artifact.zip`
-`rm -rf "#{tmp}"`
-
-# ===========================================================================
-puts "New Pharo downloaded in #{destination}"
-`open "#{full_path}"`
+openImage("New Pharo downloaded in #{destination}", full_path)
